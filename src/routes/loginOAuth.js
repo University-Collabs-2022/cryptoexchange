9d04d0faefeb06eab96b3a1002e3ca5c88a08da0
Raluca Carare <carare.raluca27@gmail.com>
2022-04-27 10:04:04 +0300

    Updated login to register provider

--- src/routes/loginOAuth.js
+++ src/routes/loginOAuth.js
@@ -1,6 +1,5 @@
 var express = require("express");
 const Users = require("../models/users.js");
-const constants = require("../constants/values.js");
 
 const server = express();
 
@@ -8,15 +7,17 @@ server.use(express.json());
 
 server.post('/auth/login',
   async (req, res) =>{
-    const { username } = req.body
-    const user = await Users.findOne({ username });
+    const { username, password } = req.body;
+    const user = await Users.findOne({ username, password });
     if (!user) {
       res.status(401).json({
         message: "Login not successful",
         error: "401: User not found",
       });
-      // res.redirect(constants.UNAUTHORIZED_URL);
+
     } else {
+
+      if(user.provider === "register") {
       await Users.updateOne(user, { lastLogin: new Date() });
       res.status(200).json({
         message: "Login successful",
@@ -24,6 +25,7 @@ server.post('/auth/login',
       })
       // res.redirect(`/api/users/${user.id}`);
     }
+  }
 });
 
 module.exports = server;
