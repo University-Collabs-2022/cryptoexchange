7b4731e29b24fae4672e98bab642aa05977b26a4
Flavia31 <ciui.alexandra31@gmail.com>
2022-04-27 19:35:16 +0300

    update user schema indexes

--- src/models/users.js
+++ src/models/users.js
@@ -22,8 +22,8 @@ const usersSchema = mongoose.Schema({
   lastLogin: { type: Date, default: Date.now },
 });
 
-usersSchema.index({ userId: 1, username: 1 });
-usersSchema.index({ userId: 1, email: 1 });
+usersSchema.index({ username: 1 });
+usersSchema.index({ email: 1 });
 
 const Users = mongoose.model("users", usersSchema);
 
