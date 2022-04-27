ab2ccb13c554b8fa05fd859f55370f1eabfe80d6
Raluca Carare <carare.raluca27@gmail.com>
2022-04-27 10:50:28 +0300

    Ecrypted password for comparing

--- src/routes/loginOAuth.js
+++ src/routes/loginOAuth.js
@@ -1,29 +1,41 @@
 var express = require("express");
 const Users = require("../models/users.js");
 
+var encrypt = require("../services/encryptPassword");
+
 const server = express();
 
 server.use(express.json());
 
-server.post('/auth/login',
-  async (req, res) =>{
-    const { username, password } = req.body;
-    const user = await Users.findOne({ username, password });
-    if (!user) {
-      res.status(401).json({
-        message: "Login not successful",
-        error: "401: User not found",
+server.post("/auth/login", async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = await Users.findOne({ username });
+  if (!user) {
+    res.status(401).json({
+      message: "Login not successful",
+      error: "401: User not found",
+    });
+  } else {
+    var encrPassword;
+    await encrypt.encryptPassword(password).then((encryptedPass) => {
+      encrPassword = encryptedPass;
+    });
+
+    const validPassword = await bcrypt.compare(encrPassword, user.password);
+    if (!validPassword) {
+      res.status(402).json({
+        message: "Incorrect password!",
+        error: "402: Incorrect password",
       });
-
     } else {
-
-      if(user.provider === "register") {
-      await Users.updateOne(user, { lastLogin: new Date() });
-      res.status(200).json({
-        message: "Login successful",
-        user,
-      })
-      // res.redirect(`/api/users/${user.id}`);
+      if (user.provider === "register") {
+        await Users.updateOne(user, { lastLogin: new Date() });
+        res.status(200).json({
+          message: "Login successful",
+          user,
+        });
+      }
     }
   }
 });
