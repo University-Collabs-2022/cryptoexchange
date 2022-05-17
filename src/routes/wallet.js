b41221cb31e886de10603431a9de4b4ca3462ef0
alexdarau <daraualexandru@gmail.com>
2022-05-17 13:46:09 +0300

    fix: change error message

--- src/routes/wallet.js
+++ src/routes/wallet.js
@@ -66,7 +66,7 @@ server.get("/api/wallet", async (req, res) => {
     )
     .catch((error) => {
       res.status(401).json({
-        message: "Something went wrong",
+        message: "Unauthorized access",
         error
       });
     })
