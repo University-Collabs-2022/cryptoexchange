2a7c992b29911ba8d09001e99c12f8739b22fb39
Flavia31 <ciui.alexandra31@gmail.com>
2022-05-25 15:41:12 +0300

    [BE] Create models and seeding for ratio: Update ratio schema

--- src/models/ratio.js
+++ src/models/ratio.js
@@ -1,16 +1,12 @@
 const mongoose = require("mongoose");
 
 const ratioSchema = mongoose.Schema({
-  baseCurrencyId: {
+  currencyId: {
     type: mongoose.Schema.Types.ObjectId,
     ref: "Currency",
   },
 
-  exchangeCurrencyId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "Currency",
-  },
-
+// The ratio compared to USD 
   ratio: {
     type: Number,
     required: true,
